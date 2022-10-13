325142eaa8ea6a204cb504ac768c020067149033
MSG89 <111423362+MSG89@users.noreply.github.com>
2022-10-13 16:42:57 +0300

    added func skeleton

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
-console.log('test');
+
+let calcButton = document.getElementById('calcButton')
 
 let gerbResultElement = document.getElementById('gerbResult');
 let gerbResult = Number(gerbResultElement.textContent);
 
 console.log(gerbResult);
+
+calcButton.addEventListener('click', (e) => {
+
+    e.preventDefault();
+    calculate();
+    
+
+});
+
+function calculate(gerbResult, itnResult){
+    console.log('test');
+}
+
