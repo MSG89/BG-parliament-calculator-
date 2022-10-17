a3b02af84d04267939e1e88dcca066bd02a8715a
MSG89 <111423362+MSG89@users.noreply.github.com>
2022-10-17 18:51:19 +0300

    minor adjustments

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 
-let calcButton = document.getElementById('calcButton');
-let resetButton = document.getElementById('resetButton');
+const calcButton = document.getElementById('calcButton');
+const resetButton = document.getElementById('resetButton');
 
-let gerbEl = document.getElementById('gerbResult');
-let itnEl = document.getElementById('ITNResult');
-let dbEl = document.getElementById('dbResult');
-let vazrazhdaneEl = document.getElementById('vazrazhdaneResult');
-let bgvazhodEl = document.getElementById('bgvazhodResult');
-let ppEl = document.getElementById('ppResult');
-let dpsEl = document.getElementById('dpsResult');
-let bspEl = document.getElementById('bspResult');
+const gerbEl = document.getElementById('gerbResult');
+const itnEl = document.getElementById('ITNResult');
+const dbEl = document.getElementById('dbResult');
+const vazrazhdaneEl = document.getElementById('vazrazhdaneResult');
+const bgvazhodEl = document.getElementById('bgvazhodResult');
+const ppEl = document.getElementById('ppResult');
+const dpsEl = document.getElementById('dpsResult');
+const bspEl = document.getElementById('bspResult');
 
 const table = document.querySelector("table");
-let chartEl = document.getElementById('chart');
+const chartEl = document.getElementById('chart');
 
 
 calcButton.addEventListener('click', (e) => {
     e.preventDefault();
 
-    let turnOut = document.getElementById('Turnout').value;
+    const turnOut = document.getElementById('Turnout').value;
 
     let gerbResult = Number(gerbEl.value);
     let itnResult = Number(itnEl.value);
@@ -36,11 +36,11 @@ calcButton.addEventListener('click', (e) => {
     let partyResults = calculate(gerbResult, itnResult, dbResult, vazrazhdaneResult, bgvazhodResult, ppResult, dpsResult, bspResult);
 
     document.querySelector('.inputField').style.display = 'none';
-    document.querySelector('.resultField').style.display = 'inline';
+    document.querySelector('.resultField').style.display = 'block';
 
 
     if (partyResults[8] > 100) {
-        document.querySelector('.inputField').style.display = 'inline';
+        document.querySelector('.inputField').style.display = 'block';
         document.querySelector('.resultField').style.display = 'none';
 
         clearFields();
@@ -48,8 +48,11 @@ calcButton.addEventListener('click', (e) => {
     }
 
     //creates table
-createTable(partyResults);
-drawPieChart(partyResults);
+    createTable(partyResults);
+    drawPieChart(partyResults);
+    calculateMajorities(partyResults);
+
+
 
 
 });
@@ -58,7 +61,7 @@ drawPieChart(partyResults);
 resetButton.addEventListener('click', (e) => {
     e.preventDefault();
 
-    document.querySelector('.inputField').style.display = 'inline';
+    document.querySelector('.inputField').style.display = 'block';
     document.querySelector('.resultField').style.display = 'none';
     table.innerHTML = '';
     clearFields();
@@ -86,13 +89,13 @@ function calculate(party1, party2, party3, party4, party5, party6, party7, party
     }
 
     let totalValidVote = 0;
-    let validPercentageArr = [party1, party2, party3, party4, party5, party6, party7, party8]; 
+    let validPercentageArr = [party1, party2, party3, party4, party5, party6, party7, party8];
     for (let i = 0; i < validPercentageArr.length; i++) {
-        if(validPercentageArr[i] < 4){
+        if (validPercentageArr[i] < 4) {
             validPercentageArr[i] = 0;
-        }else{
+        } else {
             totalValidVote += validPercentageArr[i];
-        } 
+        }
     }
 
 
@@ -121,41 +124,15 @@ function calculate(party1, party2, party3, party4, party5, party6, party7, party
     let bspPercentage = (dpsSeats / 240) * 100;
 
 
-    // let gerbSeats = (240 * party1) / 100;
-    // let gerbPercentage = (gerbSeats / 240) * 100;
-
-    // let itnSeats = (240 * party2) / 100;
-    // let itnPercentage = (itnSeats / 240) * 100;
-
-    // let dbSeats = (240 * party3) / 100;
-    // let dbPercentage = (dbSeats / 240) * 100;
-
-    // let vazrazhdaneSeats = (240 * party4) / 100;
-    // let vazrazhdanePercentage = (vazrazhdaneSeats / 240) * 100;
-
-    // let bgvazhodSeats = (240 * party5) / 100;
-    // let bgvazhodPercentage = (bgvazhodSeats / 240) * 100;
-
-    // let ppSeats = (240 * party6) / 100;
-    // let ppPercentage = (ppSeats / 240) * 100;
-
-    // let dpsSeats = (240 * party7) / 100;
-    // let dpsPercentage = (dpsSeats / 240) * 100;
-
-    // let bspSeats = (240 * party7) / 100;
-    // let bspPercentage = (dpsSeats / 240) * 100;
-
-
-
     let partyResults = [
-        party1 = ['GERB', Math.round(gerbSeats), Math.round(gerbPercentage)],
-        party2 = ['ITN', Math.round(itnSeats), Math.round(itnPercentage)],
-        party3 = ['DB', Math.round(dbSeats), Math.round(dbPercentage)],
-        party4 = ['VAZRAZHDANE', Math.round(vazrazhdaneSeats), Math.round(vazrazhdanePercentage)],
-        party5 = ['BG VAZHOD', Math.round(bgvazhodSeats), Math.round(bgvazhodPercentage)],
-        party6 = ['PP', Math.round(ppSeats), Math.round(ppPercentage)],
-        party7 = ['DPS', Math.round(dpsSeats), Math.round(dpsPercentage)],
-        party8 = ['BSP', Math.round(bspSeats), Math.round(bspPercentage)],
+        ['GERB', Math.round(gerbSeats), Math.round(gerbPercentage)],
+        ['ITN', Math.round(itnSeats), Math.round(itnPercentage)],
+        ['DB', Math.round(dbSeats), Math.round(dbPercentage)],
+        ['VAZRAZHDANE', Math.round(vazrazhdaneSeats), Math.round(vazrazhdanePercentage)],
+        ['BG VAZHOD', Math.round(bgvazhodSeats), Math.round(bgvazhodPercentage)],
+        ['PP', Math.round(ppSeats), Math.round(ppPercentage)],
+        ['DPS', Math.round(dpsSeats), Math.round(dpsPercentage)],
+        ['BSP', Math.round(bspSeats), Math.round(bspPercentage)],
         total = totalPercent
     ];
 
@@ -200,15 +177,12 @@ function createTable(partyResults) {
 
 
 function drawPieChart(partyResults) {
-    
-    let canvasEl = document.createElement('canvas')
-    canvasEl.setAttribute('id','pieChart');
-    canvasEl.setAttribute('class','resultField');
-    canvasEl.style.width = '50%';
-    canvasEl.style.maxWidth = '500px';
+
+    let canvasEl = document.createElement('canvas');
+    canvasEl.setAttribute('id', 'pieChart');
     chartEl.appendChild(canvasEl);
- 
-    
+
+
     var xValues = [partyResults[0][0], partyResults[1][0], partyResults[2][0], partyResults[3][0], partyResults[4][0], partyResults[5][0], partyResults[6][0], partyResults[7][0]];
     var yValues = [partyResults[0][2], partyResults[1][2], partyResults[2][2], partyResults[3][2], partyResults[4][2], partyResults[5][2], partyResults[6][2], partyResults[7][2]];
     var barColors = ["red", "green", "blue", "orange", "brown", "violet", "yellow", "crimson"];
@@ -231,6 +205,34 @@ function drawPieChart(partyResults) {
     });
 }
 
+function calculateMajorities(partyResults) {
+
+ 
+    //to do - make an KVP object from party Results
+ 
+
+    const combinations = new Set();
+    const threshold = 121;
+
+    const result = {};
+
+    // Object.entries(testData).map(([key1, value1]) => {
+    //     return Object.entries(testData).map(([key2, value2]) => {
+    //         if (key1 === key2) return; // party1 === party1 do nothing
+
+    //         // take care of the doubles, party1 + party2 === party2 + party1
+    //         const key = [key1, key2].sort().join(":");
+    //         if (combinations.has(key)) return;
+    //         combinations.add(key);
+
+    //         // check if the sum is over the threshold
+    //         const sum = value1 + value2;
+    //         if (sum > threshold) result[key] = sum;
+    //     });
+    // });
+
+    // console.log(result);
 
 
 
+}
