f58db2498e5d17d4d8c1ec251878b8a903e1ad0e
MSG89 <111423362+MSG89@users.noreply.github.com>
2022-12-07 18:10:11 +0200

    added party name customization + optimization

--- app.js
+++ app.js
@@ -6,20 +6,36 @@ const calcButton2 = document.getElementById('calcButton2');
 const calcButton3 = document.getElementById('calcButton3');
 const calcButton4 = document.getElementById('calcButton4');
 
-const gerbEl = document.getElementById('gerbResult');
-const itnEl = document.getElementById('ITNResult');
-const dbEl = document.getElementById('dbResult');
-const vazrazhdaneEl = document.getElementById('vazrazhdaneResult');
-const bgvazhodEl = document.getElementById('bgvazhodResult');
-const ppEl = document.getElementById('ppResult');
-const dpsEl = document.getElementById('dpsResult');
-const bspEl = document.getElementById('bspResult');
+const party1El = document.getElementById('party1result');
+const party1nameEl = document.getElementById('party1name');
+
+const party2El = document.getElementById('party2result');
+const party2nameEl = document.getElementById('party2name');
+
+const party3El = document.getElementById('party3result');
+const party3nameEl = document.getElementById('party3name');
+
+const party4El = document.getElementById('party4result');
+const party4nameEl = document.getElementById('party4name');
+
+const party5El = document.getElementById('party5result');
+const party5nameEl = document.getElementById('party5name');
+
+const party6El = document.getElementById('party6result');
+const party6nameEl = document.getElementById('party6name');
+
+const party7El = document.getElementById('party7result');
+const party7nameEl = document.getElementById('party7name');
+
+const party8El = document.getElementById('party8result');
+const party8nameEl = document.getElementById('party8name');
 
 const partyTableEl = document.getElementById('partyTable');
 const coalitionTableEl = document.getElementById('coalitionTable');
 const chartEl = document.getElementById('chart');
 const chartCoalEl = document.getElementById('chartCoalition');
 
+let wrongInput = false;
 let coalitionSize = 0;
 
 calculatePartyResults();
@@ -62,119 +78,150 @@ resetButton.addEventListener('click', (e) => {
     chartEl.innerHTML = '';
 });
 
-function calculatePartyResults(){
+function calculatePartyResults() {
     calcButton.addEventListener('click', (e) => {
         e.preventDefault();
-    
+        wrongInput = false;
         //const turnOut = document.getElementById('Turnout').value;
-    
-        let gerbResult = Number(gerbEl.value);
-        let itnResult = Number(itnEl.value);
-    
-        let dbResult = Number(dbEl.value);
-        let vazrazhdaneResult = Number(vazrazhdaneEl.value);
-    
-        let bgvazhodResult = Number(bgvazhodEl.value);
-        let ppResult = Number(ppEl.value);
-    
-        let dpsResult = Number(dpsEl.value);
-        let bspResult = Number(bspEl.value);
-    
+
+        let party1result = Number(party1El.value);
+        const party1name = party1nameEl.value;
+
+        let party2result = Number(party2El.value);
+        const party2name = party2nameEl.value;
+
+        let party3result = Number(party3El.value);
+        const party3name = party3nameEl.value;
+
+        let party4result = Number(party4El.value);
+        const party4name = party4nameEl.value;
+
+        let party5result = Number(party5El.value);
+        const party5name = party5nameEl.value;
+
+        let party6result = Number(party6El.value);
+        const party6name = party6nameEl.value;
+
+        let party7result = Number(party7El.value);
+        const party7name = party7nameEl.value;
+
+        let party8result = Number(party8El.value);
+        const party8name = party8nameEl.value;
+
         //returning arr of arr - partyNUM [partyName, seats, percentage]
-        partyResults = calculate(gerbResult, itnResult, dbResult, vazrazhdaneResult, bgvazhodResult, ppResult, dpsResult, bspResult);
-    
+        partyResults = calculate(
+            party1result, party1name,
+            party2result, party2name,
+            party3result, party3name,
+            party4result, party4name,
+            party5result, party5name,
+            party6result, party6name,
+            party7result, party7name,
+            party8result, party8name);
+
         document.querySelector('.inputField').style.display = 'none';
         document.querySelector('.resultField').style.display = 'block';
         document.querySelector('.coalitionField').style.display = 'inline-flex';
         document.querySelector('.buttons').style.display = 'block';
-    
-    
-        if (partyResults[8] > 100) {
+
+        if (wrongInput) {
             document.querySelector('.inputField').style.display = 'block';
             document.querySelector('.resultField').style.display = 'none';
             document.querySelector('.coalitionField').style.display = 'none';
             document.querySelector('.buttons').style.display = 'none';
-    
+
             clearFields();
             return
         }
-    
+
         //creates table
         createTable(partyResults);
         drawPieChart(partyResults);
-    
+
     });
 };
 
 function clearFields() {
-    document.getElementById('gerbResult').value = '';
-    document.getElementById('ITNResult').value = '';
-    document.getElementById('dbResult').value = '';
-    document.getElementById('vazrazhdaneResult').value = '';
-    document.getElementById('bgvazhodResult').value = '';
-    document.getElementById('ppResult').value = '';
-    document.getElementById('dpsResult').value = '';
-    document.getElementById('bspResult').value = '';
+    document.getElementById('party1result').value = '';
+    document.getElementById('party2result').value = '';
+    document.getElementById('party3result').value = '';
+    document.getElementById('party4result').value = '';
+    document.getElementById('party5result').value = '';
+    document.getElementById('party6result').value = '';
+    document.getElementById('party7result').value = '';
+    document.getElementById('party8result').value = '';
 }
 
-function calculate(party1, party2, party3, party4, party5, party6, party7, party8) {
+function calculate(
+    party1, party1name,
+    party2, party2name,
+    party3, party3name,
+    party4, party4name,
+    party5, party5name,
+    party6, party6name,
+    party7, party7name,
+    party8, party8name) 
+    {
 
-    let totalPercent = party1 + party2 + party3 + party4 + party5 + party6 + party7 + party8;
-    if (totalPercent > 100) {
-        ;
-        alert('percentages must be equal or less than 100');
-    }
-
-    let totalValidVote = 0;
-    let validPercentageArr = [party1, party2, party3, party4, party5, party6, party7, party8];
-    for (let i = 0; i < validPercentageArr.length; i++) {
-        if (validPercentageArr[i] < 4) {
-            validPercentageArr[i] = 0;
-        } else {
-            totalValidVote += validPercentageArr[i];
+        let totalPercent = party1 + party2 + party3 + party4 + party5 + party6 + party7 + party8;
+        if (totalPercent > 100) {
+            alert('total percentage result must be equal or less than 100');
+            wrongInput = true;
+        }else if(totalPercent < 4){
+            alert('at least one party must have more than 4 percent of the vote');
+            wrongInput = true;
         }
+
+        let totalValidVote = 0;
+        let validPercentageArr = [party1, party2, party3, party4, party5, party6, party7, party8];
+        for (let i = 0; i < validPercentageArr.length; i++) {
+            if (validPercentageArr[i] < 4) {
+                validPercentageArr[i] = 0;
+            } else {
+                totalValidVote += validPercentageArr[i];
+            }
+        }
+
+
+        let party1Seats = (240 * validPercentageArr[0]) / totalValidVote;
+        let party1Percentage = (party1Seats / 240) * 100;
+
+        let party2Seats = (240 * validPercentageArr[1]) / totalValidVote;
+        let party2Percentage = (party2Seats / 240) * 100;
+
+        let party3Seats = (240 * validPercentageArr[2]) / totalValidVote;
+        let party3Percentage = (party3Seats / 240) * 100;
+
+        let party4Seats = (240 * validPercentageArr[3]) / totalValidVote;
+        let party4Percentage = (party4Seats / 240) * 100;
+
+        let party5Seats = (240 * validPercentageArr[4]) / totalValidVote;
+        let party5Percentage = (party5Seats / 240) * 100;
+
+        let party6Seats = (240 * validPercentageArr[5]) / totalValidVote;
+        let party6Percentage = (party6Seats / 240) * 100;
+
+        let party7Seats = (240 * validPercentageArr[6]) / totalValidVote;
+        let party7Percentage = (party7Seats / 240) * 100;
+
+        let party8Seats = (240 * validPercentageArr[7]) / totalValidVote;
+        let party8Percentage = (party8Seats / 240) * 100;
+
+
+        let partyResults = [
+            [party1name, Math.round(party1Seats), Math.round(party1Percentage), party1],
+            [party2name, Math.round(party2Seats), Math.round(party2Percentage), party2],
+            [party3name, Math.round(party3Seats), Math.round(party3Percentage), party3],
+            [party4name, Math.round(party4Seats), Math.round(party4Percentage), party4],
+            [party5name, Math.round(party5Seats), Math.round(party5Percentage), party5],
+            [party6name, Math.round(party6Seats), Math.round(party6Percentage), party6],
+            [party7name, Math.round(party7Seats), Math.round(party7Percentage), party7],
+            [party8name, Math.round(party8Seats), Math.round(party8Percentage), party8],
+            total = totalPercent
+        ];
+
+        return partyResults
     }
-
-
-    let gerbSeats = (240 * validPercentageArr[0]) / totalValidVote;
-    let gerbPercentage = (gerbSeats / 240) * 100;
-
-    let itnSeats = (240 * validPercentageArr[1]) / totalValidVote;
-    let itnPercentage = (itnSeats / 240) * 100;
-
-    let dbSeats = (240 * validPercentageArr[2]) / totalValidVote;
-    let dbPercentage = (dbSeats / 240) * 100;
-
-    let vazrazhdaneSeats = (240 * validPercentageArr[3]) / totalValidVote;
-    let vazrazhdanePercentage = (vazrazhdaneSeats / 240) * 100;
-
-    let bgvazhodSeats = (240 * validPercentageArr[4]) / totalValidVote;
-    let bgvazhodPercentage = (bgvazhodSeats / 240) * 100;
-
-    let ppSeats = (240 * validPercentageArr[5]) / totalValidVote;
-    let ppPercentage = (ppSeats / 240) * 100;
-
-    let dpsSeats = (240 * validPercentageArr[6]) / totalValidVote;
-    let dpsPercentage = (dpsSeats / 240) * 100;
-
-    let bspSeats = (240 * validPercentageArr[7]) / totalValidVote;
-    let bspPercentage = (bspSeats / 240) * 100;
-
-
-    let partyResults = [
-        ['ГЕРБ', Math.round(gerbSeats), Math.round(gerbPercentage), party1],
-        ['ИТН', Math.round(itnSeats), Math.round(itnPercentage), party2],
-        ['ДБ', Math.round(dbSeats), Math.round(dbPercentage), party3],
-        ['Възраждане', Math.round(vazrazhdaneSeats), Math.round(vazrazhdanePercentage), party4],
-        ['БГ Възход', Math.round(bgvazhodSeats), Math.round(bgvazhodPercentage), party5],
-        ['ПП', Math.round(ppSeats), Math.round(ppPercentage), party6],
-        ['ДПС', Math.round(dpsSeats), Math.round(dpsPercentage), party7],
-        ['БСП', Math.round(bspSeats), Math.round(bspPercentage), party8],
-        total = totalPercent
-    ];
-
-    return partyResults
-}
 
 function createTable(partyResults) {
     let captionEl = document.createElement('caption');
@@ -249,15 +296,16 @@ function drawPieChart(partyResults) {
 
 function calculatePartyCoalitions(partyResults, coalitionSize) {
 
+
     let inputData = {
-        ГЕРБ: partyResults[0][1],
-        ИТН: partyResults[1][1],
-        ДБ: partyResults[2][1],
-        Възраждане: partyResults[3][1],
-        БГВъзход: partyResults[4][1],
-        ПП: partyResults[5][1],
-        ДПС: partyResults[6][1],
-        БСП: partyResults[7][1]
+        [partyResults[0][0]]: partyResults[0][1],
+        [partyResults[1][0]]: partyResults[1][1],
+        [partyResults[2][0]]: partyResults[2][1],
+        [partyResults[3][0]]: partyResults[3][1],
+        [partyResults[4][0]]: partyResults[4][1],
+        [partyResults[5][0]]: partyResults[5][1],
+        [partyResults[6][0]]: partyResults[6][1],
+        [partyResults[7][0]]: partyResults[7][1]
     };
 
     const keysRaw = Object.keys(inputData);
@@ -279,18 +327,18 @@ function calculatePartyCoalitions(partyResults, coalitionSize) {
 
     //possible coalitions algorithm.
 
-    if(coalitionSize == 2){
+    if (coalitionSize == 2) {
         for (let i = 0; i < keys.length; i++) {
 
             for (let j = 0; j < keys.length; j++) {
-    
+
                 if (keys[i] === keys[j]) {
                     continue;
                 } else if ((values[i] + values[j]) > majorityThreshold) {
-    
+
                     let currentCombo = [keys[i], keys[j]].sort().join(" + ");
                     let currentComboSeats = [values[i] + values[j]];
-    
+
                     if (Coalitions.includes(currentCombo)) {
                         continue;
                     } else {
@@ -301,19 +349,19 @@ function calculatePartyCoalitions(partyResults, coalitionSize) {
             }
         }
 
-    }else if(coalitionSize == 3){
+    } else if (coalitionSize == 3) {
         for (let i = 0; i < keys.length; i++) {
 
             for (let j = 0; j < keys.length; j++) {
-    
+
                 for (let k = 0; k < keys.length; k++) {
                     if (keys[i] === keys[j] || keys[i] === keys[k] || keys[j] === keys[k]) {
                         continue;
                     } else if ((values[i] + values[j] + values[k]) > majorityThreshold) {
-    
+
                         let currentCombo = [keys[i], keys[j], keys[k]].sort().join(" + ");
                         let currentComboSeats = [values[i] + values[j] + values[k]];
-    
+
                         if (Coalitions.includes(currentCombo)) {
                             continue;
                         } else {
@@ -325,21 +373,21 @@ function calculatePartyCoalitions(partyResults, coalitionSize) {
             }
         }
 
-    }else if(coalitionSize == 4){
+    } else if (coalitionSize == 4) {
         for (let i = 0; i < keys.length; i++) {
 
             for (let j = 0; j < keys.length; j++) {
-    
+
                 for (let k = 0; k < keys.length; k++) {
-    
+
                     for (let l = 0; l < keys.length; l++) {
                         if (keys[i] === keys[j] || keys[i] === keys[k] || keys[i] === keys[l] || keys[j] === keys[k] || keys[j] === keys[l] || keys[k] === keys[l]) {
                             continue;
                         } else if ((values[i] + values[j] + values[k] + values[l]) > majorityThreshold) {
-    
+
                             let currentCombo = [keys[i], keys[j], keys[k], keys[l]].sort().join(" + ");
                             let currentComboSeats = [values[i] + values[j] + values[k] + values[l]];
-    
+
                             if (Coalitions.includes(currentCombo)) {
                                 continue;
                             } else {
@@ -352,7 +400,7 @@ function calculatePartyCoalitions(partyResults, coalitionSize) {
             }
         }
     }
-   
+
     createTablePartyCoalitions(Coalitions, CoalitionSeats, coalitionSize)
 }
 
@@ -365,9 +413,9 @@ function createTablePartyCoalitions(coalitionName, totalSeats, coalitionSize) {
 
     if (coalitionSize == 2) {
         tableCaption = 'Possible two party coalitions';
-    }else if(coalitionSize == 3){
+    } else if (coalitionSize == 3) {
         tableCaption = 'Possible three party coalitions';
-    }else if(coalitionSize == 4){
+    } else if (coalitionSize == 4) {
         tableCaption = 'Possible four party coalitions';
     }
 
